a1913f9f32c5189bdb394fb889be4fd01da5f862
Jessica <2160475@my.ipleiria.pt>
2017-11-07 16:09:06 +0000

    Segundo commit

---
+++ app/src/main/java/amsi/dei/estg/ipleiria/pt/projeto/DetalhesMensaActivity.java
@@ -0,0 +1,14 @@
+package amsi.dei.estg.ipleiria.pt.projeto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetalhesMensaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes_mensa);
+        setTitle("Detalhes mesa");
+    }
+}
