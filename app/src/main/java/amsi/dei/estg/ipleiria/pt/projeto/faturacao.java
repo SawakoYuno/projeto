25f0036a18b2d78ee0a4dd2d842d91288c1ae2fd
Jessica <2160475@my.ipleiria.pt>
2017-11-21 15:57:03 +0000

    jessica gracio antoes é topo

---
+++ app/src/main/java/amsi/dei/estg/ipleiria/pt/projeto/faturacao.java
@@ -0,0 +1,33 @@
+package amsi.dei.estg.ipleiria.pt.projeto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class faturacao extends AppCompatActivity {
+    private String getArray_spinnerPagamentos[];
+    private String array_spinnerIva[];
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faturacao);
+
+        /*----------PRENCHER O SPINNER DE TIPO DE PAGAMENTOS---------*/
+        getArray_spinnerPagamentos=new String[3];
+        getArray_spinnerPagamentos[0]="Dinheiro";
+        getArray_spinnerPagamentos[1]="Cartão de crédito";
+        getArray_spinnerPagamentos[2]="Cheque";
+        Spinner sPagamentos = (Spinner) findViewById(R.id.spinnerPagamentos);
+        ArrayAdapter adapterPagamentos = new ArrayAdapter(this, android.R.layout.simple_spinner_item, getArray_spinnerPagamentos);
+        sPagamentos.setAdapter(adapterPagamentos);
+
+         /*----------PRENCHER O SPINNER DO TIPO DE IVA---------*/
+        array_spinnerIva=new String[2];
+        array_spinnerIva[0]="26%";
+        array_spinnerIva[1]="3%";
+        Spinner sIva = (Spinner) findViewById(R.id.spinnerIva);
+        ArrayAdapter adapterIva = new ArrayAdapter(this, android.R.layout.simple_spinner_item, array_spinnerIva);
+        sIva.setAdapter(adapterIva);
+    }
+}
