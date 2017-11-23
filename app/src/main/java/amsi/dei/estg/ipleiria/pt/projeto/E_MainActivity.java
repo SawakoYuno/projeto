872f19db870da221f6651eae602acc4ba9f9ad2c
Vieira <2160451@my.ipleiria.pt>
2017-11-23 11:46:31 +0000

    Mudanças no layout

--- app/src/main/java/amsi/dei/estg/ipleiria/pt/projeto/E_MainActivity.java
+++ app/src/main/java/amsi/dei/estg/ipleiria/pt/projeto/E_MainActivity.java
@@ -3,7 +3,7 @@ package amsi.dei.estg.ipleiria.pt.projeto;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class E_MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
