e5f91286cc0070768ca87df736f33c1a28626908
Vieira <2160451@my.ipleiria.pt>
2017-11-23 12:31:37 +0000

    main cliente

---
+++ app/src/main/java/amsi/dei/estg/ipleiria/pt/projeto/c_itemListaementa.java
@@ -0,0 +1,13 @@
+package amsi.dei.estg.ipleiria.pt.projeto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class c_itemListaementa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_c_item_listaementa);
+    }
+}
