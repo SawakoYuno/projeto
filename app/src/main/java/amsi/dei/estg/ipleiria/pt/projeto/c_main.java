72ccbcef198ddf921c8bc41f0f597f10e8a525d8
Vieira <2160451@my.ipleiria.pt>
2017-11-23 12:00:53 +0000

    E maiusculo

--- app/src/main/java/amsi/dei/estg/ipleiria/pt/projeto/c_main.java
+++ app/src/main/java/amsi/dei/estg/ipleiria/pt/projeto/c_main.java
@@ -3,13 +3,11 @@ package amsi.dei.estg.ipleiria.pt.projeto;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class E_MainActivity extends AppCompatActivity {
+public class c_main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_E_main);
-        setTitle("Mesas");
+        setContentView(R.layout.activity_c_main);
     }
 }
-//helllooooo
