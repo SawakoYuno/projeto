6ebb2deaa87ec2e7093f3ba94cf98bd2748130c0
SawakoYuno <2160475@my.ipleiria.pt>
2017-12-27 16:07:45 +0000

    xpto

--- app/src/main/java/amsi/dei/estg/ipleiria/pt/projeto/e_pedidos.java
+++ app/src/main/java/amsi/dei/estg/ipleiria/pt/projeto/e_pedidos.java
@@ -4,11 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class e_pedidos extends AppCompatActivity {
+import java.util.List;
+
+import listeners.ArtigoListener;
+import modelo.Artigo;
+
+public class e_pedidos extends AppCompatActivity implements ArtigoListener {
 
     public static final String btn = "BTN";
     private TextView txtNmesa;
     private Bundle extra;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,17 @@ public class e_pedidos extends AppCompatActivity {
        txtNmesa.setText(extra.getString("BTN"));
 
 
+    }
+
+    @Override
+    public void onRefreshListaArtigos(List<Artigo> listaArtigo) {
+        adaptador = new ListaAAdapter(getApplication(), lista);
+        listaLivros.setAdapter(adaptador);
+        adaptador.refresh(lista);
+    }
+
+    @Override
+    public void onUpdateListaArtigosBD(Artigo artigo, int operacao) {
 
     }
 }
